8e3406ca1ce874bc44211b1d495add743a2cdf28
Lordtence <tedwardlanglie@gmail.com>
2022-05-08 01:52:02 -0700

    Added 2 models, one "intraDayRespose" to hold the data for the View, from the parsed JSON as strings. Then userInput as a model to hold the string for user response on Index view.

---
+++ RequestPrototype/Models/intraDayResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace RequestPrototype.Models
+{
+    public class intraDayResponse
+    {
+        // this model should hold the data that will be passed to a view. Holds the results of the JSON response (after parse of course)
+        public string time { get; set; }
+        public string open { get; set; }
+        public string high { get; set; }
+        public string low { get; set; }
+        public string close { get; set; }
+        public string volume { get; set; }
+    }
+}
---
+++ RequestPrototype/Models/userInput.cs
@@ -0,0 +1,8 @@
+﻿namespace RequestPrototype.Models
+{
+    public class userInput
+    {
+        // holds the user response from the index page
+        public string? Ticker { get; set; }
+    }
+}
